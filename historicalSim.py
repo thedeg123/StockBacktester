42b97d9befb48052ecada352f2756a48a384c16e
David <dgold@oberlin.edu>
2018-10-17 22:05:11 -0400

    broke things down

--- historicalSim.py
+++ historicalSim.py
@@ -7,7 +7,7 @@ from pandas.tseries.holiday import USFederalHolidayCalendar
 import pandas as pd
 import sys
 import matplotlib as plt
-COMPVAL = 'spx'
+COMPVAL = 'SPY'
 
 def scrubDate(date):
     '''
@@ -18,17 +18,15 @@ def scrubDate(date):
     cal = USFederalHolidayCalendar()
     holidays = cal.holidays(start=(today - relativedelta(years=5)), end=today).to_pydatetime()
     today=datetime.today().strftime('%Y-%m-%d')
-    uDate = date if not pd.isnull(date) else \
-    datetime.strptime(today, '%Y-%m-%d')
+    uDate = date if isinstance(date,datetime) else datetime.strptime(today, '%Y-%m-%d')
     while(uDate in holidays):
-        #print("Date:", uDate.strftime('%Y-%m-%d'), "was a holiday. Instead using ", end="")
-        uDate= uDate - timedelta(days=1)
-        #print(uDate)
+        #If date was a holiday
+        uDate = uDate - timedelta(days=1)
     if uDate.weekday()>4:
-        #print("Date:", uDate.strftime('%Y-%m-%d'), "was a weekend. Instead using ", end="")
+        #If date was a weekend
         uDate = uDate - timedelta(days=(uDate.weekday()-4))
-        #print(uDate)
     return uDate
+
 def __fixDates__(data):
     '''
     Updates each date to be not a weekend or Holiday
@@ -38,27 +36,40 @@ def __fixDates__(data):
     for (i,sell) in enumerate(data['Sell']):
         data.ix[i,'Sell']= scrubDate(sell)
     return data
-
-def __printBeta__(data, userData):
-    netChange = 0;
-    lowd = datetime.today();
-    newd = datetime(1000,1,1)
+def __getMetrics__(userData, data):
+    '''
+    Takes in a dataframe and gets metrics for it. Helper program for __calculateMetrics__
+    '''
+    ticker = userData.index[0]
+    userData["purchaseVal"] = data.ix[0,"close"] * userData["Shares"]
+    userData["currentVal"] = data.ix[1,"close"] * userData["Shares"]
+    #Getting the beta
+    userData["Beta"] = (userData["currentVal"] - userData["purchaseVal"]).round(3)
+    userData["pBeta"] = userData["Beta"] / data.ix[0,"close"]
+    return userData
+def __calculateMetrics__(data, userData):
+    '''
+    Prints portfolio metrics.
+    '''
     for (ticker,data) in data:
-        startd= scrubDate(userData.ix[ticker, "Buy"])
-        endd = scrubDate(userData.ix[ticker, "Sell"])
-        change = ((data.ix[1,"close"] - data.ix[0,"close"] -0) * userData.ix[ticker, "Shares"]).round(3)
-        netChange+=change
-        lowd = min(lowd,startd)
-        newd = max(newd,endd)
-        print(ticker, "Change of: ", change, "over", (endd - startd).days, "days with",\
+        print(userData)
+        userData.update(__getMetrics__(userData, data))
+        startd = userData.ix[ticker, "Buy"]
+        endd = userData.ix[ticker, "Sell"]
+        print(ticker, "Change of: ", userData.ix[ticker, "Beta"] , "over", (endd - startd).days, "days with",\
         userData.ix[ticker, "Shares"], "shares.")
-    print("Net change of:",netChange, "over", (newd-lowd).days, "days.")
-def genBeta(file,comp):
+
+    print("Net change of:",userData["Beta"].sum(), "over", (userData['Sell'].max()-userData['Buy'].min()).days, "days.")
+
+def stockRetrace(file):
+    '''
+    Finds portfolio change. comp is for alpha.
+    '''
     fin = pd.read_excel("CurrentPortfolio.xlsx")
     userData = __fixDates__(pd.DataFrame(fin, columns=fin.keys()))
     panel_data=dict()
     for index,stock in userData.iterrows():
-        endv = "\n" if (index==userData.iloc[-1:].index[0]) else "\r"
+        endv = "\n" if (index==userData.iloc[-1:].index[0]) else "\n"
         print("Retreaving data for:",stock.name,end=endv)
         try:
             dates = [pdr.DataReader(stock.name,'iex', start=stock.Buy, end=stock.Buy), \
@@ -67,7 +78,11 @@ def genBeta(file,comp):
         except ValueError:
             print("The data for \"", stock.name, "\" are incomplete. Continuing without.")
             continue;
-    __printBeta__(panel_data.items(), userData)
+    __calculateMetrics__(panel_data.items(), userData)
+    #Generating alpha based off spy (s&p index) and when first stock was bought and last was sold.
+    print(userData)
+
+    compalph = pdr.DataReader(COMPVAL,'iex', start=userData['Buy'].min(), end=userData['Sell'].max())
     #print(panel_data, userData.head())
 if __name__=='__main__':
-    genBeta(sys.argv[1:], COMPVAL)
+    stockRetrace(sys.argv[1:])
